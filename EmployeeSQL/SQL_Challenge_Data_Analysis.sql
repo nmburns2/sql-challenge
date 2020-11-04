5f20771945ee3c2d1e37636409e27421c89f33a3
Nicholas Burns <nmburns@triad.rr.com>
2020-11-04 18:45:26 -0500

    edit analysis

--- EmployeeSQL/SQL_Challenge_Data_Analysis.sql
+++ EmployeeSQL/SQL_Challenge_Data_Analysis.sql
@@ -5,7 +5,7 @@ FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no;
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date 
 FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
